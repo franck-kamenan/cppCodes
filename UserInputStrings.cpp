2c25961e33a5afe200ca8245f2e9e0996d854f7c
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-30 17:18:03 +0200

    User Input Strings

---
+++ UserInputStrings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string prenom;
+	cout << "Entre ton prenom: \n";
+	cin >> prenom;
+	cout << "Ton prenom est: " << prenom << "\n";
+
+	return 0;
+}
