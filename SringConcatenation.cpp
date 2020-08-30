0340ee8f0f11c7cb2bf408f5f7a6776f39f4c317
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-30 14:21:15 +0200

    String Concatenation

---
+++ SringConcatenation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#include <string>
+using namespace std;
+
+int main()
+{
+	string prenom = "John ";
+	string nom = "Doe";
+	string nomComplet = prenom + nom;
+	cout << nomComplet << "\n";
+
+	return 0;
+}
