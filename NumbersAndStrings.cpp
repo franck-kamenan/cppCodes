278a472164ac7431deb565c0b7e123ad6b9d24ca
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-30 14:38:36 +0200

    Numbers And Strings

---
+++ NumbersAndStrings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include <string>
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = a + b;
+	cout << c << "\n";
+
+	string d = "10";
+	string e = "20";
+	string f = d + e;
+	cout << f << "\n";
+
+	return 0;
+}
