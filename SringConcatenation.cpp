5a7b0e359ab3d9fbcbe0932dc908b83e06f13ef0
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-30 14:25:54 +0200

    Append

--- SringConcatenation.cpp
+++ SringConcatenation.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	string prenom = "John ";
 	string nom = "Doe";
-	string nomComplet = prenom + nom;
+	string nomComplet = prenom.append(nom);
 	cout << nomComplet << "\n";
 
 	return 0;
