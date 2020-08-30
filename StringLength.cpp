e165a4c6f0a9cb6ac1dbce4e6a2ce4cc2fcdf011
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-30 14:49:33 +0200

    String Length

---
+++ StringLength.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <string>
+using namespace std;
+
+int main()
+{
+	string texte = "ALPHABET";
+	cout << "La longueuer du string de texte est: " << texte.length() << "\n";
+	cout << "La longueuer du string de texte est: " << texte.size() << "\n";
+
+	return 0;
+}
